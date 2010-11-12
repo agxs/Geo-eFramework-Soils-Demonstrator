2344dbb706a9af51aa54108093d2176250bb6bed
aseales <aseales@fb20097c-be17-0410-985a-cd93cd8e1696>
2010-11-12 17:44:18 +0000

    Start of ProcessModelController with stub test
    git-svn-id: https://devel.edina.ac.uk/svn/geo-dev/eFramework/gefcdemo/trunk@20869 fb20097c-be17-0410-985a-cd93cd8e1696

--- src/main/java/edina/eframework/gefcdemo/controllers/ProcessErosionController.java
+++ src/main/java/edina/eframework/gefcdemo/controllers/ProcessErosionController.java
@@ -1,7 +1,14 @@
 package edina.eframework.gefcdemo.controllers;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import edina.eframework.gefcdemo.domain.SoilErosionWps;
@@ -15,9 +22,25 @@ public class ProcessErosionController {
   public void setVelocityEngine( VelocityEngine velocityEngine ) {
     this.velocityEngine = velocityEngine;
   }
+  
+  private String templateLocation;
+  
+  public void setTemplateLocation( String templateLocation ) {
+    this.templateLocation = templateLocation;
+  }
 
   @RequestMapping
-  String handleProcess( SoilErosionWps params ) {
+  public String handleProcess( SoilErosionWps params ) throws IOException {
+    
+    Writer wpsRequest = new StringWriter();
+    Map<String, Object> velocityMap = new HashMap<String,Object>();
+    
+    velocityMap.put( "test", "a url" );
+    
+    VelocityEngineUtils.mergeTemplate( velocityEngine, templateLocation, velocityMap, wpsRequest );
+    wpsRequest.close();
+    
+    System.out.println( wpsRequest.toString() );
     // generate wps request
     // store data
     // run WmsConfigurationGenerator
---
+++ src/test/java/edina/eframework/gefcdemo/controllers/test/ProcessErosionControllerTest.java
@@ -0,0 +1,25 @@
+package edina.eframework.gefcdemo.controllers.test;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import edina.eframework.gefcdemo.controllers.ProcessErosionController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/applicationContext.xml",
+                       "file:src/main/webapp/WEB-INF/gefcdemo-servlet.xml"})
+public class ProcessErosionControllerTest {
+
+  @Autowired
+  private ProcessErosionController processController;
+  
+  @Test
+  public void testHandleProcess() throws IOException {
+    processController.handleProcess( null );
+  }
+}
