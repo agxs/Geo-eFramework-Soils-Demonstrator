eb7322733c729d1542c0dcb57e5adc702de605bd
aseales <aseales@fb20097c-be17-0410-985a-cd93cd8e1696>
2010-11-12 17:44:15 +0000

    Testing Controllers the Spring Way
    git-svn-id: https://devel.edina.ac.uk/svn/geo-dev/eFramework/gefcdemo/trunk@20868 fb20097c-be17-0410-985a-cd93cd8e1696

--- src/test/java/edina/eframework/gefcdemo/controllers/test/MapControllerTest.java
+++ src/test/java/edina/eframework/gefcdemo/controllers/test/MapControllerTest.java
@@ -3,14 +3,23 @@ package edina.eframework.gefcdemo.controllers.test;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import edina.eframework.gefcdemo.controllers.MapController;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/applicationContext.xml",
+                       "file:src/main/webapp/WEB-INF/gefcdemo-servlet.xml"})
 public class MapControllerTest {
 
+  @Autowired
+  private MapController mapController;
+  
   @Test
   public void testHandleMap() {
-    MapController mapController = new MapController();
     String viewName = mapController.handleMap();
     assertEquals( "Unexpected view name returned from MapController.",
                   "gefcdemo", viewName ); 
